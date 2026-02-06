477859785c3a51fe846496923a829f662b785873
LindaWadaa <wdaalinda424@gmail.com>
2026-02-06 17:05:13 +0100

    Mise à jour dynamique des partenaires, ajout de catégories et validation du formulaire de contact

---
+++ assets/js/data.js
@@ -0,0 +1,231 @@
+/**
+ * Data file for Dari Partners
+ * Edit this file to add or modify partners.
+ */
+const partnersData = [
+    {
+        category: 'graphic', // Architects
+        name: 'Maissa Jannene',
+        contact: '55453467',
+        address: 'Nabeul',
+        page: 'https://www.instagram.com/mj.archi6/',
+        image: './assets/img/log1.jpg'
+    },
+    {
+        category: 'graphic', // Architects
+        name: 'Chamsassil Belgl',
+        contact: '21235254',
+        address: 'Tunis',
+        page: '',
+        image: './assets/img/services-01.jpg'
+    },
+    // Add more partners here following the same structure
+    {
+        category: 'conseil', // Ingénieur Conseil
+        name: 'Hedi Ayed Lakhal',
+        contact: '22556177',
+        address: 'Nabeul',
+        page: 'http://www.facebook.com/ayedlahkalhedi',
+        image: './assets/img/services-02.jpg'
+    },
+    {
+        category: 'conseil', // Ingénieur Conseil
+        name: 'BC Engeneering',
+        contact: '21023464',
+        address: 'Nabeul',
+        page: 'http://www.bc-engineering.tn/',
+        image: './assets/img/services-02.jpg'
+    },
+    {
+        category: 'conseil', // Ingénieur Conseil
+        name: 'Youssef Adel',
+        contact: '98395000',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-02.jpg'
+    },
+    {
+        category: 'topographe', // Topographe
+        name: 'Mohamedexpert',
+        contact: '97385119',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-01.jpg'
+    },
+    {
+        category: 'topographe', // Topographe
+        name: 'BEAT.GEO OFFIC',
+        contact: '58886672',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-01.jpg'
+    },
+    {
+        category: 'topographe', // Topographe
+        name: 'GEO3D',
+        contact: '22414111',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-01.jpg'
+    },
+    {
+        category: 'constructeur', // Constructeur
+        name: 'N@K de baimen',
+        contact: '97364866',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-03.jpg'
+    },
+    {
+        category: 'constructeur', // Constructeur
+        name: 'Zariat Batiment',
+        contact: '',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-03.jpg'
+    },
+    {
+        category: 'constructeur', // Constructeur
+        name: 'Wiem Batiment',
+        contact: '',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-03.jpg'
+    },
+    {
+        category: 'constructeur', // Constructeur
+        name: 'Rekaya Batiment',
+        contact: '',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-03.jpg'
+    },
+    {
+        category: 'beton', // Béton
+        name: 'CAP BETON',
+        contact: '27322000',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'beton', // Béton
+        name: 'Béton Sahel',
+        contact: '72388069',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'beton', // Béton
+        name: 'Daoued Béton',
+        contact: '71388208',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'beton', // Béton
+        name: 'Betonia',
+        contact: '36186000',
+        address: 'Nabeul',
+        page: 'http://www.betonia.tn/',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'beton', // Béton
+        name: 'Mayar Béton',
+        contact: '26433775',
+        address: 'Grombalia',
+        page: 'https://www.facebook.com/profile.php?id=100063499676591',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'plombier', // Plombier
+        name: 'Mehdi Ghannay',
+        contact: '58821651',
+        address: 'Nabeul',
+        page: 'http://www.instagram.com/pro_plumbing_services/',
+        image: './assets/img/services-05.jpg'
+    },
+    {
+        category: 'plombier', // Plombier
+        name: 'Malek Jlassi',
+        contact: '51182977',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-05.jpg'
+    },
+    {
+        category: 'plombier', // Plombier
+        name: 'Aziz Limem',
+        contact: '20227974',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-05.jpg'
+    },
+    {
+        category: 'plombier', // Plombier
+        name: 'Hedi Bouzekri',
+        contact: '98264004',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-05.jpg'
+    },
+    {
+        category: 'plombier', // Plombier
+        name: 'Mohamed Ben Aziza',
+        contact: '',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-05.jpg'
+    },
+    {
+        category: 'electricien', // Electricien
+        name: 'Hafath',
+        contact: '',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'electricien', // Electricien
+        name: 'Ilyes Lagili',
+        contact: '29 230 451',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'electricien', // Electricien
+        name: 'Mohamed Zamm',
+        contact: '27 353 376',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'electricien', // Electricien
+        name: 'Ala Rjab',
+        contact: '97979697',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-04.jpg'
+    },
+    {
+        category: 'jardinier', // Jardinier
+        name: 'Cap Vert',
+        contact: '72369453',
+        address: 'Nabeul',
+        page: 'https://capvert.com.tn/',
+        image: './assets/img/services-08.jpg'
+    },
+    {
+        category: 'marbre', // Marbre
+        name: 'Majidi',
+        contact: '98224305',
+        address: 'Nabeul',
+        page: '',
+        image: './assets/img/services-01.jpg'
+    }
+];
